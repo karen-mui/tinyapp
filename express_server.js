7888065e79f3e792489452572568b802fb1643c6
Karen Mui <karen_mui@hotmail.com>
2022-11-30 03:39:00 +0000

    filled out with html skeleton

--- express_server.js
+++ express_server.js
@@ -3,10 +3,16 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
